cfe28966ec52522a2ac17f7ab8d4a6c9ec0504e6
viccwq <vic_cwq@163.com>
2017-02-20 22:52:10 +0800

    add

--- fpc/main.cpp
+++ fpc/main.cpp
@@ -7,19 +7,21 @@ int fpc_circle(Mat &img)
     split(img, channels);
     Mat img_r = channels[2];
     GaussianBlur(img_r, img_r, Size(7,7), 2);
-    threshold(img_r, img_r, 40, 255, CV_THRESH_BINARY);
+	img_r = img_r * 1.5;
+//    threshold(img_r, img_r, 40, 255, CV_THRESH_BINARY);
+
 
     vector<Vec3f> circles;
-    HoughCircles(img_r, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 50);
+    HoughCircles(img_r, circles, CV_HOUGH_GRADIENT, 1, 50, 150, 60);
 
     for (int i = 0; i < circles.size(); i++)
     {
         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
         int radius = cvRound(circles[i][2]);
-        circle(img, center, 3, Scalar(0,255,0),-1);
+        circle(img, center, 3, Scalar(0,255,0), 2);
         circle(img, center,radius, Scalar(0,0,255), 3);
     }
-
+	imwrite("./fpc2_ret.jpg", img);
     return 0;
 }
 
